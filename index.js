c6dbeb0ead9fa8b4e44427f7ee9e4ca51e0f11ad
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-01-23 16:43:20 +0100

    Append grid to body

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let body = document.querySelector("body");
 let grid = document.createElement("div");
 let square = document.createElement("div");
 
+body.appendChild(grid);
 grid.appendChild(square);
