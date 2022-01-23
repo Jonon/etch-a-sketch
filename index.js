dd52cbed5cfe3b38607022465db665a6fe38bfb6
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-01-23 18:40:52 +0100

    create responsive square

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let body = document.querySelector("body");
 let grid = document.createElement("div");
 grid.classList.add("grid");
 let square = document.createElement("div");
+square.classList.add("square");
 
 body.appendChild(grid);
 grid.appendChild(square);
