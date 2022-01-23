21b5c2cd5db6cef46a474cebfc68dbb270d5fc5f
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-01-23 18:31:47 +0100

    Create grid
    Create a 16x16 grid of squares

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let body = document.querySelector("body");
 
 let grid = document.createElement("div");
+grid.classList.add("grid");
 let square = document.createElement("div");
 
 body.appendChild(grid);
