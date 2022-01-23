35d4bcc840505c607a7a98b76d258b914089bed4
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-01-23 19:48:27 +0100

    Add createGrid
    Add function CreateGrid. Create a maximum number of squares in the grid based on the input number

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ let body = document.querySelector("body");
 
 let grid = document.createElement("div");
 grid.classList.add("grid");
-let square = document.createElement("div");
-square.classList.add("square");
+
+function createSquare(maxSquares, grid) {
+  let appendSquareToGrid;
+
+  for (let i = 1; i <= maxSquares; i++) {
+    square = document.createElement("div");
+    square.setAttribute("index", i);
+    square.classList.add("square");
+
+    appendSquareToGrid += grid.appendChild(square);
+  }
+}
+
+createSquare(16 * 16, grid);
 
 body.appendChild(grid);
-grid.appendChild(square);
