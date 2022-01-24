ae2b842fe099c0bcdd7745c3d3dafc2df05135f2
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-01-24 15:19:49 +0100

    Refactor createSquare
    push all elements into an array and remove id attribute because it is not needed

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let body = document.querySelector("body");
+let body = document.querySelector(".container");
 
 let grid = document.createElement("div");
 grid.classList.add("grid");
 
 function createSquare(maxSquares, grid) {
-  let appendSquareToGrid;
+  let squares = [];
 
   for (let i = 1; i <= maxSquares; i++) {
-    square = document.createElement("div");
-    square.setAttribute("index", i);
+    let square = document.createElement("div");
     square.classList.add("square");
-
-    appendSquareToGrid += grid.appendChild(square);
+    grid.appendChild(square);
+    // Push all new squares into an array called squares
+    squares.push(square);
   }
+  return squares;
 }
 
-createSquare(16 * 16, grid);
+let squares = createSquare(256, grid);
 
 body.appendChild(grid);
